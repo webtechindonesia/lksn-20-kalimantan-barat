5058bc892d2146321b6c60e64bc6dd2cfaf15f04
Erik <erikhau3010@gmail.com>
2020-10-22 09:28:27 +0700

    commit 3

--- module_client_side/src/Game.js
+++ module_client_side/src/Game.js
@@ -3,10 +3,26 @@ class Game {
     this.time = 0;
     this.score = 0;
 
+    this.init();
     this.listener();
   }
 
-  listener() {}
+  init() {}
+
+  listener() {
+    inputName.addEventListener("keyup", (e) => {
+      if (e.target.value == "") startBtn.disabled = true;
+      else startBtn.disabled = false;
+    });
+
+    startBtn.addEventListener("click", () => {
+      this.start();
+    });
+  }
+
+  start() {
+    instructions.classList.remove("visible");
+  }
 
   update() {}
 }
--- module_client_side/src/main.js
+++ module_client_side/src/main.js
@@ -3,6 +3,10 @@ const ctx = canvas.getContext("2d");
 const w = canvas.width;
 const h = canvas.height;
 
+const startBtn = document.querySelector(".btn-start");
+const inputName = document.querySelector(".form-group input");
+const instructions = document.querySelector(".instructions");
+
 window.onload = init();
 
 function init() {
